eb7d8167adda5321db04a0f44418b1e06f4f6fbb
Yaroslav <yaroslav.mim@gmail.com>
2020-03-11 10:16:25 +0300

    lab1

---
+++ ds-1-master/src/PostForm/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PostForm.Models;
+using Grpc.Net.Client;
+using BackendApi;
+
+namespace PostForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+ 
+        [HttpPost]
+        public async Task<ActionResult> Create(string JobText)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Job.JobClient(channel);
+            var reply = await client.RegisterAsync(
+                              new RegisterRequest { Description = JobText });
+            var text = "Job Id: " + reply.Id + " Job Description: " + JobText;
+           return Ok(text);
+        } 
+    }
+}
